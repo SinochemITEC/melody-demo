025146a0c8b3c0da24f8183b48431a06851f1c61
Chang <15335713+iamajar@users.noreply.github.com>
2018-07-09 17:32:33 +0800

    部分Utils

--- src/main/java/com/eyeieye/melody/demo/web/action/utils/UtilsAction.java
+++ src/main/java/com/eyeieye/melody/demo/web/action/utils/UtilsAction.java
@@ -15,27 +15,28 @@ import java.util.Date;
 public class UtilsAction {
 
     @RequestMapping("demo/DateUtil")
-    public void dateUtilPage(User user,Integer afterDays, Integer beforeDays, ModelMap modelMap){
+    public void dateUtilPage(User user, Integer afterDays, Integer beforeDays, ModelMap modelMap) {
         //获取当前日期时间
-        String nowDateTime = DateUtil.getDateTime(DateUtil.getDatePattern(),new Date());
+        String nowDateTime = DateUtil.getDateTime(DateUtil.getDatePattern(), new Date());
         //获取今天是星期几：
         int weeks = DateUtil.getDay(new Date());
         //获取几天后的日期
-        Date nextDate =null;
-        if((afterDays == null || afterDays == 0) == false) {
+        Date nextDate = null;
+        if ((afterDays == null || afterDays == 0) == false) {
             nextDate = DateUtil.getRelativeDate(new Date(), afterDays);
-            modelMap.put("nextWeeks", DateUtil.getDay(nextDate));
+            modelMap.put("nextWeeks", getWeeks(DateUtil.getDay(nextDate)));
+            modelMap.put("afterDays",afterDays);
         }
         //获取几天前的日期
         Date lastDate = null;
-        if(!(beforeDays == null || beforeDays == 0)) {
-            lastDate = DateUtil.getRelativeDate(new Date(), beforeDays*-1);
-            modelMap.put("lastWeeks", DateUtil.getDay(lastDate));
+        if (!(beforeDays == null || beforeDays == 0)) {
+            lastDate = DateUtil.getRelativeDate(new Date(), beforeDays * -1);
+            modelMap.put("lastWeeks", getWeeks(DateUtil.getDay(lastDate)));
+            modelMap.put("beforeDays",beforeDays);
         }
 
         modelMap.put("dateNow", nowDateTime);
-        modelMap.put("weeks", weeks);
-
+        modelMap.put("weeks", getWeeks(weeks));
 
 
     }
@@ -49,5 +50,26 @@ public class UtilsAction {
 
         System.out.println(DateUtil.getDatePattern());
 
+    }
+
+    private String getWeeks(int weeks) {
+        switch (weeks) {
+            case 0:
+                return "星期日";
+            case 1:
+                return "星期一";
+            case 2:
+                return "星期二";
+            case 3:
+                return "星期三";
+            case 4:
+                return "星期四";
+            case 5:
+                return "星期五";
+            case 6:
+                return "星期六";
+            default:
+                return "未知";
+        }
     }
 }
